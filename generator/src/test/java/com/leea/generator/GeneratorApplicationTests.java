282817822616e56b1526d771d14455488d049714
EllaMcGlynn <ellamcglynn3@gmail.com>
2025-05-30 11:07:26 +0100

    initial commit

---
+++ generator/src/test/java/com/leea/generator/GeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leea.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
