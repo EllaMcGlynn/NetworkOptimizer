89124306418f053d0a9634eae294f2922723bdbb
Laila Byadi Semia <A00325361@student.tus.ie>
2025-06-03 16:08:01 +0100

    Added AUTO and MANUAL mode.

--- optimizer/src/main/java/com/leea/optimizer/controller/OptimizationController.java
+++ optimizer/src/main/java/com/leea/optimizer/controller/OptimizationController.java
@@ -1,7 +1,9 @@
 package com.leea.optimizer.controller;
 
 import com.leea.optimizer.models.OptimizationRecommendation;
+import com.leea.optimizer.models.OptimizerMode;
 import com.leea.optimizer.service.OptimizationService;
+import com.leea.optimizer.service.OptimizerModeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,8 +16,21 @@ public class OptimizationController {
     @Autowired
     private OptimizationService optimizer;
 
+    @Autowired
+    private OptimizerModeService modeService;
+
     @GetMapping("/recommendations")
     public List<OptimizationRecommendation> getRecommendations() {
         return optimizer.optimize();
+    }
+
+    @GetMapping("/mode")
+    public OptimizerMode getMode() {
+        return modeService.getCurrentMode();
+    }
+
+    @PostMapping("/mode")
+    public void setMode(@RequestBody String mode) {
+        modeService.setMode(OptimizerMode.valueOf(mode.toUpperCase()));
     }
 }
