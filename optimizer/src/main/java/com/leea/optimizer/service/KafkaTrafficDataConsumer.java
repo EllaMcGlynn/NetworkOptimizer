7db8e8cf7e100764acbe546a50b95789a2416561
A00185551 <A00185551@host.docker.internal>
2025-06-04 16:53:36 +0100

    added error handling and logger to optimizer consumer

--- optimizer/src/main/java/com/leea/optimizer/service/KafkaTrafficDataConsumer.java
+++ optimizer/src/main/java/com/leea/optimizer/service/KafkaTrafficDataConsumer.java
@@ -1,8 +1,10 @@
 package com.leea.optimizer.service;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.leea.logger.OptimizerLogger;
 import com.leea.optimizer.models.TrafficData;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -17,11 +19,20 @@ public class KafkaTrafficDataConsumer {
 
     @KafkaListener(topics = "resource-usage-data", groupId = "optimizer-group")
     public void listen(String message) throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
-        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        TrafficData data = mapper.readValue(message, TrafficData.class);
-        recentData.add(data);
+    	try {
+	        ObjectMapper mapper = new ObjectMapper();
+	        mapper.registerModule(new JavaTimeModule());
+	        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+	        TrafficData data = mapper.readValue(message, TrafficData.class);
+	        if (data.getResourceAllocated() == null || data.getResourceUsage() == null || data.getTimestamp() == null) {
+		    	OptimizerLogger.logError("null value found entry for node " + data.getNodeId() + " skipped");
+		    }else {
+		    	recentData.add(data);
+		    	OptimizerLogger.log(data);
+		    }
+    	}catch (JsonParseException  e) {
+			e.printStackTrace();
+		}
     }
 
     public List<TrafficData> getRecentData() {
