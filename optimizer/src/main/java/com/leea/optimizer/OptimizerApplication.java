72c1263df5041604d8056b459f9553062539d85f
EllaMcGlynn <ellamcglynn3@gmail.com>
2025-06-05 20:53:30 +0100

    ui merged

--- optimizer/src/main/java/com/leea/optimizer/OptimizerApplication.java
+++ optimizer/src/main/java/com/leea/optimizer/OptimizerApplication.java
@@ -3,9 +3,11 @@ package com.leea.optimizer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableKafka
+@EnableScheduling
 public class OptimizerApplication {
     public static void main(String[] args) {
         SpringApplication.run(OptimizerApplication.class, args);
